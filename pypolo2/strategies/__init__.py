47f75aafb0d34947b526000a8941ca499f0f0808
xiaozijianx <xiaozijianx@163.com>
2023-09-26 16:43:37 +0800

    update

--- pypolo2/strategies/__init__.py
+++ pypolo2/strategies/__init__.py
@@ -1,37 +1,7 @@
-from .active_sampling import ActiveSampling
-from .active_planning import ActivePlanning
-from .bezier import Bezier
-from .myopic_planning import MyopicPlanning
-from .myopic_lattice_planning import MyopicLatticePlanning
-from .random_sampling import RandomSampling
-from .myopic_planning_mi import MyopicPlanningMI
-from .myopic_lattice_planning_mi import MyopicLatticePlanningMI
-from .myopic_lattice_planning_mi_sprinkler import MyopicLatticePlanningMISprinkler
 from .nonmyopic_lattice_planning_mi_sprinkler import NonMyopicLatticePlanningMISprinkler
-from .nonmyopic_lattice_planning_mi_sprinkler_control import NonMyopicLatticePlanningMISprinklerControl
-from .nonmyopic_lattice_planning_mi_sprinkler_control_mifix import NonMyopicLatticePlanningMISprinklerControlFix
-from .traversal_lattice_planning_mi_sprinkler_control import TraversalLatticePlanningMISprinklerControl
-from .time_traversal_lattice_planning_mi_sprinkler_control import TimeTraversalLatticePlanningMISprinklerControl
-from .traversal_lattice_planning_mi_sprinkler_control_selecttoall import TraversalLatticePlanningMISprinklerControlSelecttoALL
-from .SA_lattice_planning_mi_sprinkler_control import SALatticePlanningMISprinklerControl
 from .strategy import IStrategy
 
 __all__ = [
-    "ActiveSampling",
-    "ActivePlanning",
-    "Bezier",
-    "MyopicPlanning",
-    "MyopicLatticePlanning"
-    "RandomSampling",
-    "MyopicPlanningMI",
-    "MyopicLatticePlanningMI"
-    "MyopicLatticePlanningMISprinkler"
     "NonMyopicLatticePlanningMISprinkler"
-    "NonMyopicLatticePlanningMISprinklerControl"
-    "NonMyopicLatticePlanningMISprinklerControlFix"
-    "TraversalLatticePlanningMISprinklerControl"
-    "TraversalLatticePlanningMISprinklerControlSelecttoALL"
-    "TimeTraversalLatticePlanningMISprinklerControl"
-    "SALatticePlanningMISprinklerControl"
     "IStrategy",
 ]
